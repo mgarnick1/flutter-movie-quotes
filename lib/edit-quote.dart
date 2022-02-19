dfa8e918814e6acc4f6fdbd91813fd67dde75d52
mgarnick1 <markgman008@gmail.com>
2022-02-19 13:51:17 -0700

    added the edit and delete functionality

--- lib/edit-quote.dart
+++ lib/edit-quote.dart
@@ -6,12 +6,26 @@ import 'quote.dart';
 
 Future<Map<String, dynamic>> editQuote(Quote? body) async {
   return await http
-      .post(Uri.parse('http://localhost:3001/quotes/${body?.id}'),
+      .put(Uri.parse('http://localhost:3001/quotes/${body?.id}'),
           headers: {"Content-Type": "application/json"}, body: jsonEncode(body))
       .then((http.Response response) {
     final int statusCode = response.statusCode;
     if (statusCode < 200 || statusCode > 400) {
       throw Exception("Error Editing Quote");
+    }
+    Map<String, dynamic> newQuote = json.decode(response.body);
+    return newQuote;
+  });
+}
+
+Future<Map<String, dynamic>> deleteQuote(Quote? body) async {
+  return await http
+      .delete(Uri.parse('http://localhost:3001/quotes/${body?.id}'),
+          headers: {"Content-Type": "application/json"}, body: jsonEncode(body))
+      .then((http.Response response) {
+    final int statusCode = response.statusCode;
+    if (statusCode < 200 || statusCode > 400) {
+      throw Exception("Error deleting Quote");
     }
     Map<String, dynamic> newQuote = json.decode(response.body);
     return newQuote;
@@ -68,27 +82,71 @@ class _EditQuoteState extends State<EditQuote> {
               _buildFormInputs(
                   'Please enter the actors name', _formKey, authorController),
               Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Editing Quote')));
-                      Quote newQuote = new Quote(
-                          quote: quoteController.text,
-                          author: authorController.text,
-                          actor: actorController.text);
-                      await editQuote(newQuote);
-                      quoteController.clear();
-                      authorController.clear();
-                      actorController.clear();
-                      Navigator.pushNamed(context, '/list');
-                    }
-                  },
-                  child: const Text('Edit Quote'),
-                ),
-              )
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Editing Quote')));
+                            Quote newQuote = new Quote(
+                                id: widget.quote.id,
+                                quote: quoteController.text,
+                                author: authorController.text,
+                                actor: actorController.text,
+                                movieId: widget.quote.movieId);
+                            await editQuote(newQuote);
+                            quoteController.clear();
+                            authorController.clear();
+                            actorController.clear();
+                            Navigator.pushNamed(context, '/list');
+                          }
+                        },
+                        child: const Text('Edit Quote'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => showDialog(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                                  title: const Text('Delete Quote'),
+                                  content: const Text(
+                                      'Are you sure you want to delete this quote?'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                        onPressed: () async {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(const SnackBar(
+                                                  content:
+                                                      Text('Deleted Quote')));
+                                          Quote removeQuote = new Quote(
+                                              id: widget.quote.id,
+                                              quote: quoteController.text,
+                                              author: authorController.text,
+                                              actor: actorController.text,
+                                              movieId: widget.quote.movieId);
+                                          await deleteQuote(removeQuote);
+                                          quoteController.clear();
+                                          authorController.clear();
+                                          actorController.clear();
+                                          Navigator.pushNamed(context, '/list');
+                                        },
+                                        child: const Text('Yes')),
+                                    TextButton(
+                                        onPressed: () =>
+                                            Navigator.pop(context, 'Cancel'),
+                                        child: const Text('No'))
+                                  ],
+                                ))
+
+                        // await editQuote(newQuote);
+                        ,
+                        child: const Text('Delete Quote'),
+                      )
+                    ],
+                  ))
             ],
           ),
         ));
